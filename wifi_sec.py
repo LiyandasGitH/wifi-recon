97eb39cf7eb7f3c126fb126c79d532159c78b256
liyanda <tonisiliyanda@gmail.com>
2026-09-10 10:03:57 +0200

    feat: added comments and created scan wifi func, draft of parse_network_info

--- wifi_sec.py
+++ wifi_sec.py
@@ -5,13 +5,12 @@ import csv
 import json 
 import subprocess
 
-from datetime import datetime
-# sniff wifi packets capture 
-from scapy.all import sniff
-# scapy 802.11 wifi layer
-from scapy.layers.dot11 import *
-from tabulate import tabulate 
+from datetime import datetime 
+from scapy.all import sniff # sniff wifi packets capture 
+from scapy.layers.dot11 import * # scapy 802.11 wifi layer
+from tabulate import tabulate  # to make it pretty 
 
+# project folders
 DATA_DIR = 'data'
 LOGS_DIR = 'logs'
 REPORTS_DIR = 'reports'
@@ -20,16 +19,19 @@ os.makedirs(DATA_DIR, exist_ok=True)
 os.makedirs(LOGS_DIR, exist_ok=True)
 os.makedirs(REPORTS_DIR, exist_ok=True)
 
-DEFAULT_INTERFACE = None
-SCAN_TIMEOUT = 10
+# configuration
+DEFAULT_INTERFACE = None # use monitor mode interface
+SCAN_TIMEOUT = 10 # seconds to scan networks
 LOG_FILE = os.path.join(LOGS_DIR, 'wifi_tool.log')
 RESULTS_FILE = os.path.join(DATA_DIR, 'networks.csv')
 
+# tool information
 TOOL_NAME = 'wifi security recon tool'
 TOOL_VERSION = '1.0'
 AUTHOR = 'liyanda'
 DESCRIPTION = 'Find nearby wifi networks and analyse security details'
 
+# main menu 
 def show_menu():
     subprocess.run(
         'cls' if os.name == 'nt' else 'clear',
@@ -57,4 +59,23 @@ def get_wifi_interface():
         return interfaces
 
 # func : scan wifi networks
+def scan_wifi(interface, timeout = 5):
+    networks = []
+    print(f'[*] Scanning on {interface}...PLease wait {timeout}s')
+    os.system(f'iwlist {interface} scan | tee scan_output.txt') # perform scan & save output 
+    time.sleep(timeout)
+    with open ('scan_output.txt', 'r', errors='ignore') as f:
+        data = f.read().split('Cell') # split each cell/network block
+        for cell in data:
+            if 'ESSID:' in cell:
+                network = parse_network_info(cell) # parse details from each block
+                if network:
+                    networks.append(network)
+    return networks
+
+# func : parse network information
+def parse_network_info(cell):   # extract useful info from scan block
+    network = {}
+    network['BSSID'] = re.search(r'Address:\s*([0-9A-Fa-f:]{17})', cell)
+    
 
